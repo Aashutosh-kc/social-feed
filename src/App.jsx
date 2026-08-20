9254d21a60bf099405d1c1a542e1b4e29441b79f
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-08-20 19:38:08 +0545

    feat: render posts from an array using map

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 import Post from './components/Posts'
 export default function App(){
 
+  const posts = [
+  { id: 1, author: "Aashutosh", text: "My first post", likes: 4 },
+  { id: 2, author: "Someone Else", text: "Another post", likes: 2 },
+  { id: 3, author: "Third User", text: "3r post", likes: 7 },
+]
 
   return(
   <>
-    <Post author="Aashutosh-kc" text="My first Post" likes={3}  />
+    {posts.map((n) =>(<Post key={n.id} author={n.author} text={n.text} likes={n.likes}  />))}
   </>
   )
 }
