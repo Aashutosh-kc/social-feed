950310ff619600a3dbdf92b145c04faa79b0edf7
Aashutosh-kc <aashutoshkc853@gmail.com>
2026-08-22 13:42:03 +0545

    feat: add comments per Posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Post from './components/Posts'
 
 export default function App(){
 
-  const [posts,setPosts] = useState([{ id: 1, author: "Aashutosh", text: "My first post", likes: 4 },
-    { id: 2, author: "Someone Else", text: "Another post", likes: 2 }]);
+  const [posts,setPosts] = useState([{ id: 1, author: "Aashutosh", text: "My first post", likes: 4,comments: [] },
+    { id: 2, author: "Someone Else", text: "Another post", likes: 2 ,comments: []}]);
  
   const [postText,setPostText] = useState('');
 
@@ -15,11 +15,17 @@ export default function App(){
       id: Date.now(),
       author: "Aashutosh KC",
       text: postText,
-      likes: 0
+      likes: 0,
+      comments: []
     }
     setPosts([newPost,...posts])
     setPostText('')
   }
+
+  function addComment(id,newValue){
+    setPosts((n)=>n.map((post) =>{ return(post.id===id ? {...post,comments: [...post.comments,newValue]} : post ) }))
+  }
+
 
   return(
   <>
@@ -28,7 +34,7 @@ export default function App(){
       onChange={(e)=>setPostText(e.target.value)} onKeyDown={(e) => {e.key==="Enter" && handleAdd()}}/>
       <button onClick={handleAdd}>Add</button>
     </div>
-    {posts.map((n) =>(<Post key={n.id} author={n.author} text={n.text} likes={n.likes}  />))}
+    {posts.map((n) =>(<Post key={n.id} author={n.author} id={n.id} text={n.text} likes={n.likes} addComment={addComment} comments={n.comments}/>))}
   </>
   )
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
-export default function Post({author,text,likes}){
-
+export default function Post({id,author,text,likes,addComment,comments}){
+    const [newInputComment,setNewInputComment] = useState('');
     const [likeCount,setLikeCount] = useState(likes);
 
     function handleLikeCount(){
@@ -14,6 +14,16 @@ export default function Post({author,text,likes}){
             <p>{text}</p>
             <div>Likes: {likeCount}</div>
             <button onClick={handleLikeCount}>Like</button>
+            <input type="text" placeholder="Add a comment" value={newInputComment} 
+            onKeyDown={(e)=>{
+                if(e.key==="Enter"){ 
+                    if (newInputComment==='') return;
+                    addComment(id,newInputComment);
+                    setNewInputComment('');
+                }
+            }} 
+            onChange={(e) => setNewInputComment(e.target.value)}/>
+            {comments.map((comment,index) => (<li key={index} >{comment}</li>))}
         </>
     )
 }
